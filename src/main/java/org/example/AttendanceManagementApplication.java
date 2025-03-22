34452b32faa87f4b165752c90906484757e3a810
Sonali Sidana <sonalisidana@Sonalis-MacBook-Air.local>
2025-03-22 23:36:17 +0530

    Initialised spring project

---
+++ src/main/java/org/example/AttendanceManagementApplication.java
@@ -0,0 +1,12 @@
+// AttendanceManagementApplication.java
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceManagementApplication.class, args);
+    }
+}
